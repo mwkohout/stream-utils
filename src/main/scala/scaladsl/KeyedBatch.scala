6862e9549dc1f48fca76d7f025ee062d07d858b0
Michael Kohout <mwkohout@gmail.com>
2022-10-09 16:48:31 -0500

    renamed to KeyedBatch.  Let's have it actually batch things.  It's more useful for kafka based systems, since a whole batch either succeeds or fails (with the ability to handle values with ```recover```).
    Another operator can group and output in order of completion.

--- src/main/scala/scaladsl/KeyedBatch.scala
+++ src/main/scala/scaladsl/KeyedBatch.scala
@@ -11,56 +11,57 @@ import java.time.Duration
 import scala.util.{Failure, Success}
 import java.util.concurrent.ConcurrentMap
 import java.util.concurrent.CompletionStage
-import scala.concurrent.{ExecutionContextExecutor, Future}
+import scala.concurrent.{ExecutionContext, ExecutionContextExecutor, Future}
 import scala.jdk.FutureConverters.*
 import scala.jdk.javaapi
-import scala.concurrent.Future
 import scala.concurrent.duration.FiniteDuration
 
-object KeyedForkJoin{
+object KeyedBatch{
 
-  def apply[I,K,ICOLLECTION,OFUTURE,O](
+  def apply[I,K,ICOLLECTION,OFUTURE,O, OCOLLECTION](
                                         maxElements:Int,
                                         batchWait:FiniteDuration,
                                         key:I=>K,
                                         batchEvaluator:(K,ICOLLECTION)=>OFUTURE)
                                       (using toFuture:Conversion[OFUTURE,Future[O]])
-                                      (using toICollection:Conversion[Seq[I],ICOLLECTION]):Flow[I,O,NotUsed] ={
+                                      (using toOCollection:Conversion[Map[K,O],OCOLLECTION])
+                                      (using toICollection:Conversion[Seq[I],ICOLLECTION]):Flow[I,OCOLLECTION,NotUsed] ={
 
     val grouped: Flow[I, Seq[I], NotUsed] = Flow.apply[I].groupedWithin(maxElements,batchWait);
 
-    val keyedForkJoin: Flow[Seq[I], Future[O], NotUsed] = Flow.fromGraph(
-      new KeyedForkJoin[I, K, ICOLLECTION, OFUTURE, O](
+    val batch: Flow[Seq[I], Future[Map[K,O]], NotUsed] = Flow.fromGraph(
+      new KeyedBatch[I, K, ICOLLECTION, OFUTURE, O](
         key, (_:ActorSystem, key:K, iCollection:ICOLLECTION)=>batchEvaluator(key,iCollection),
         toICollection,toFuture
     ));
-    grouped.via(keyedForkJoin).flatMapConcat((o:Future[O])=> Source.future(o))
+    grouped.via(batch).flatMapConcat((o:Future[Map[K,O]])=> Source.future(o)).map(toOCollection)
 
   }
 
 }
-class KeyedForkJoin[I,K,ICOLLECTION,OFUTURE,O]
+class KeyedBatch[I,K,ICOLLECTION,OFUTURE,O]
     (key: I => K,
      batchEvaluator: (ActorSystem,K, ICOLLECTION) => OFUTURE,
      toICollection:Seq[I]=>ICOLLECTION,
-     toFuture:OFUTURE=>Future[O]) extends GraphStage[FlowShape[Seq[I], Future[O]]] {
+     toFuture:OFUTURE=>Future[O]) extends GraphStage[FlowShape[Seq[I], Future[Map[K,O]]]] {
 
   val in = Inlet[Seq[I]]("KeyedForkJoin.in")
-  val out = Outlet[Future[O]]("KeyedForkJoin.out")
+  val out = Outlet[Future[Map[K,O]]]("KeyedForkJoin.out")
   override val shape = FlowShape.of(in, out)
 
   override def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
     new GraphStageLogic(shape) {
 
       val actorSystem = materializer.system
-
+      implicit val ec:ExecutionContext = actorSystem.dispatcher
       setHandler(in, new InHandler {
         override def onPush(): Unit = {
           val batch = grab(in)
-          val futures: Map[K,Future[O]] = batch.groupBy(key)
-            .map((k, seq)=>(k,toFuture(batchEvaluator(actorSystem,k,toICollection(seq)))));
-          val values = futures.values.toList
-          emitMultiple(out,values)
+          val futures: Map[K,Future[(K,O)]] = batch.groupBy(key)
+            .map((k, seq)=>(k,toFuture(batchEvaluator(actorSystem,k,toICollection(seq))).map(o=>(k,o))(actorSystem.dispatcher)));
+          val values = Future.sequence(futures.values)
+          val batchMap = values.map(results=> Map.from(results))
+          emit(out,batchMap)
         }
       })
 
